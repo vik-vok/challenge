cd9179d61d864939e59027fcf58936b9f187e3ed
nikalosa <nikalosaberidze@gmail.com>
2020-09-04 04:08:37 +0400

    Fix errors

--- functions/ChallengeUpdate.go
+++ functions/ChallengeUpdate.go
@@ -14,44 +14,49 @@ type PubSubMessage struct {
 }
 
 var Message struct {
-	ReceiverUserId string  `json:"receiverUserId"`
-	Score          float32 `json:"score"`
+	ReceiverUserId  string  `json:"receiverUserId"`
+	OriginalVoiceId string  `json:"originalVoiceId"`
+	Score           float32 `json:"score"`
 }
 
-func ChallengeUpdate(ctx context.Context, m PubSubMessage) {
+func ChallengeUpdate(ctx context.Context, m PubSubMessage) error {
 
 	data := string(m.Data) // Automatically decoded from base64.
-	log.Printf("Hello, %s!", data)
-
+	log.Printf("%s\n", data)
 	message := &Message
-	log.Println(message)
 	err := json.Unmarshal([]byte(data), message)
-	log.Printf(message.ReceiverUserId, message.Score)
+	log.Printf(message.ReceiverUserId)
+	log.Printf(message.OriginalVoiceId)
+	log.Printf("%.6f", message.Score)
 	if err != nil {
 		log.Printf("Error While Parsing Request Body!")
-		return
+		return err
 	}
 
-	// 2. Connect to database
-	client, err := datastore.NewClient(ctx, ProjectName)
+	//2. Connect to database
+	ctxDb := context.Background()
+	client, err := datastore.NewClient(ctxDb, ProjectName)
 	if err != nil {
 		fmt.Println(err) /* log error */
-		return
+		return err
 	}
 
 	// 3. Get data
 	var challenges []Challenge
-	query := datastore.NewQuery(EntityName).Filter("ReceiverUserId =", message.ReceiverUserId).Filter("Score =", message.Score)
-	ids, err := client.GetAll(ctx, query, &challenges)
+
+	query := datastore.NewQuery(EntityName).Filter("ReceiverUserId =", message.ReceiverUserId).Filter("OriginalVoiceId =", message.OriginalVoiceId)
+	ids, err := client.GetAll(ctxDb, query, &challenges)
 	accomplished := 0
+	log.Printf("%d\n", len(challenges))
 	for i := range challenges {
+		fmt.Println(challenges[i])
 		if challenges[i].Accomplished {
 			continue
 		}
 		if message.Score >= challenges[i].Score {
 			challenges[i].Accomplished = true
 			challengeKey := datastore.IDKey(EntityName, ids[i].ID, nil)
-			if _, err := client.Put(ctx, challengeKey, &challenges[i]); err != nil {
+			if _, err := client.Put(ctxDb, challengeKey, &challenges[i]); err != nil {
 				log.Fatalf("tx.Put: %v", err)
 			}
 			accomplished++
@@ -59,5 +64,5 @@ func ChallengeUpdate(ctx context.Context, m PubSubMessage) {
 
 	}
 	log.Printf("%d\n", accomplished)
-	return
+	return nil
 }
